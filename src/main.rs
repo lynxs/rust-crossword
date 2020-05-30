35fcee5a60e2eefd049dc9781f2f92aecb500b29
lynxs <lynxs-alexander@yandex.ru>
2020-05-30 22:55:54 +0700

    add blacklist getter

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ use log::*;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use thiserror::Error;
 
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+}
 
 fn main() {
     dotenv().ok();
@@ -14,25 +20,54 @@ fn main() {
     // }
 
     let key = "DICT_PATH";
-    let value = dotenv::var(key).unwrap();
-    println!("{}: {}", key, value);
+    let dict_path = dotenv::var(key).unwrap();
+    println!("{}: {}", key, dict_path);
+
+    let blacklist_path = dotenv::var("BLACKLIST_PATH").unwrap();
 
     debug!("debugging");
 
-    if let Ok(lines) = read_lines(value) {
-        // for line in lines {
-        //     if let Ok(word) = line {
-        //         //println!("{}", word);
-        //         let v: Vec<&str> = word.splitn(2, '#').collect();
-        //         println!("{}", v[0]);
-        //     }
-        // }
-        let words = lines
-                    .map(|l| l.unwrap().splitn(2, '#').map(str::to_owned).next())
-                    .filter_map(|w| w)
-                    .collect::<Vec<_>>();
-        println!("{}", words.len());
-    }
+    // if let Ok(lines) = read_lines(value) {
+    //     // for line in lines {
+    //     //     if let Ok(word) = line {
+    //     //         //println!("{}", word);
+    //     //         let v: Vec<&str> = word.splitn(2, '#').collect();
+    //     //         println!("{}", v[0]);
+    //     //     }
+    //     // }
+    //     let words = lines
+    //                 .map(|l| l.unwrap().splitn(2, '#').map(str::to_owned).next())
+    //                 .filter_map(|w| w)
+    //                 .collect::<Vec<_>>();
+    //     println!("{}", words.len());
+    // }
+    let words = get_dict_words(dict_path).unwrap();
+    println!("{} dictionary words readed", words.len());
+
+    let blacklist = get_blacklist_words(blacklist_path).unwrap();
+    println!("{} blacklist words readed", blacklist.len());
+}
+
+fn get_dict_words<P>(filename: P) -> Result<Vec<String>, MyError>
+where P: AsRef<Path>, {
+    let lines = read_lines(filename)?;
+    
+    let words = lines
+                .map(|l| l.unwrap().splitn(2, '#').map(str::to_owned).next())
+                .filter_map(|w| w)
+                .collect::<Vec<_>>();
+    Ok(words)
+}
+
+fn get_blacklist_words<P>(filename: P) -> Result<Vec<String>, MyError>
+where P: AsRef<Path>, {
+    // let lines = read_lines(filename)?;
+    
+    // let words: Vec<_> = lines
+    //             .filter_map(Result::ok)
+    //             .collect();
+    // Ok(words)
+    Ok(read_lines(filename)?.filter_map(Result::ok).collect())
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
